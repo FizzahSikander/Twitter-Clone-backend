00cee0d2116823c55ddd18f52c6ed4e77edf35a9
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-05-08 16:31:20 +0200

    Syncing branch

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    about: { type: String, default: "" },
+    occupation: { type: String, default: "" },
+    hometown: { type: String, default: "" },
+    website: { type: String, default: "" },
+    registeredAt: { type: Date, default: Date.now },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    bio: String,
+    image: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dsr0s5lbq/image/upload/v1743707427/j8ry5tkeqmdhyoxp3nrc.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
