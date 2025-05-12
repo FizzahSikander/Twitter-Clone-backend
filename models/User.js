fb2221764a47a9938502ea0f712d1675cd1ba9df
Hamoudi Lazkani <hamoudi_lazkani@hotmail.se>
2025-05-13 00:38:55 +0200

    Sync with laptop

--- models/User.js
+++ models/User.js
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    nickname: { type: String, required: true, unique: true },
     about: { type: String, default: "" },
     occupation: { type: String, default: "" },
     hometown: { type: String, default: "" },
-    website: { type: String, default: "" },
+    homepage: { type: String, default: "" },
     followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    tweets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }],
     bio: String,
     image: {
       type: String,
@@ -23,3 +25,7 @@ const userSchema = new mongoose.Schema(
 
 const User = mongoose.model("User", userSchema);
 export default User
+
+
+
+
--- routes.js
+++ routes.js
@@ -1,14 +1,38 @@
 import { Router } from "express";
-import User from "./models/User.js";
-import { handleRegister, handleLogin } from "./middlewares/handleUser.js";
+// import User from "./models/User.js";
+// import Tweet from "./models/Tweet.js"; 
+import { handleTweet } from "./middlewares/handleTweet.js";
+import { handleRegister, handleLogin, validateUser } from "./middlewares/handleUser.js";
+
+
 const router = Router();
 
+
+// Login & Registration 
+router.post("/register", handleRegister);
+router.post("/login", handleLogin);
+
+// Tweet handlers
+router.post("/tweet", validateUser, handleTweet);
+
+
+
+
+
+
+
+
+// Validation
 router.get("/", (req, res) => {
   console.log("welcome to root");
   res.json({ message: "Welcome to root" }).status(200);
 });
 
-router.post("/register", handleRegister);
-router.post("/login", handleLogin);
+router.get("/validate", validateUser, (req, res) => {
+  console.log(req.user)
+  res.status(200).json({ message: "Token is valid", ok: true, user: req.user });
+});
+
+
 
 export default router;
