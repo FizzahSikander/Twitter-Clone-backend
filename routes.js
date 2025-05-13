223192d6e2587615c1971154521b8beba398b844
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-05-13 10:08:33 +0200

    Auth

--- routes.js
+++ routes.js
@@ -1,26 +1,17 @@
 import { Router } from "express";
 // import User from "./models/User.js";
-// import Tweet from "./models/Tweet.js"; 
-import { handleTweet } from "./middlewares/handleTweet.js";
-import { handleRegister, handleLogin, validateUser } from "./middlewares/handleUser.js";
-
+// import Tweet from "./models/Tweet.js";
+import {
+  handleRegister,
+  handleLogin,
+  validateUser,
+} from "./middlewares/handleUser.js";
 
 const router = Router();
 
-
-// Login & Registration 
+// Login & Registration
 router.post("/register", handleRegister);
 router.post("/login", handleLogin);
-
-// Tweet handlers
-router.post("/tweet", validateUser, handleTweet);
-
-
-
-
-
-
-
 
 // Validation
 router.get("/", (req, res) => {
@@ -29,10 +20,8 @@ router.get("/", (req, res) => {
 });
 
 router.get("/validate", validateUser, (req, res) => {
-  console.log(req.user)
+  console.log(req.user);
   res.status(200).json({ message: "Token is valid", ok: true, user: req.user });
 });
-
-
 
 export default router;
